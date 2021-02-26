56ba02676b8d3541834e41a8341b36b221553483
Kevin Hsu <kevin_hsu@joy.com.tw>
2021-02-26 16:51:00 +0800

    Update 2021-02-26T08:50:56.509Z

---
+++ assets/js/all.js
@@ -0,0 +1,30 @@
+"use strict";
+
+window.onload = function () {
+  menubar();
+};
+
+function menubar() {
+  var el_bar = document.querySelector('[data-menubar]');
+
+  if (el_bar) {
+    el_bar.addEventListener('click', toggleMenu);
+  }
+}
+
+function toggleMenu() {
+  var el_menu = document.querySelector('[data-menu]');
+
+  if (el_menu) {
+    el_menu.classList.toggle('flex'); // 監聽 animationEnd
+
+    if (el_menu.classList.contains('flex')) {
+      el_menu.classList.add('animate-fadeIn');
+      el_menu.classList.remove('animate-fadeOut');
+    } else {
+      el_menu.classList.remove('animate-fadeIn');
+      el_menu.classList.add('animate-fadeOut');
+    }
+  }
+}
+//# sourceMappingURL=all.js.map
